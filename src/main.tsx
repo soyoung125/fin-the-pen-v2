4e62f78e1ecd0f501113f37068c29d28d719b869
soyoung125 <sypark041754@gmail.com>
2023-07-17 17:34:18 +0900

    feat: 라우터 적용

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { RouterProvider } from 'react-router-dom'
+import router from './app/router.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <App />
+    <RouterProvider router={router} />
 )
