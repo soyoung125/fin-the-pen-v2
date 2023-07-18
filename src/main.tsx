25a4d9fd0251a00aac343a7a7e9d62e17edc3ee7
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-07-19 01:12:14 +0900

    chore: msw 기본 세팅 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,25 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import router from './app/router.tsx';
 import { RouterProvider } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
+import { worker } from './mocks/browser';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <RecoilRoot>
-    <RouterProvider router={router} />
-  </RecoilRoot>
-);
+async function main() {
+  // msw 세팅 시작
+  await worker.start({
+    serviceWorker: {
+      url: '/fin-the-pen-web-v2/mockServiceWorker.js',
+    },
+    onUnhandledRequest: 'bypass',
+  });
+  // msw 세팅 끝
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <RecoilRoot>
+      <RouterProvider router={router} />
+    </RecoilRoot>
+  );
+}
+
+main();
