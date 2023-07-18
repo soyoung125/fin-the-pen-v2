5f067a1c5f5ff1c5eb3121d01bb351ed66ea88fc
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-07-19 00:54:11 +0900

    chore: RecoilRoot Provider 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
-import router from './app/router.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import router from './app/router.tsx';
+import { RouterProvider } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
+  <RecoilRoot>
     <RouterProvider router={router} />
-)
+  </RecoilRoot>
+);
