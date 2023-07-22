50159462ce13aaa45a213a17b69c3505938afeed
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-07-23 03:39:54 +0900

    feat: 로그인 페이지 헤더 추가

--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -1,6 +1,7 @@
 import Footer from "../../containers/sign/signInContainer/Footer.tsx";
 import {Stack} from "@mui/material";
 import CenterBox from "../../components/layouts/CenterBox.tsx";
+import Header from "../../containers/sign/signInContainer/Header.tsx";
 
 function SignIn() {
   return(
@@ -10,6 +11,7 @@ function SignIn() {
         alignItems="center"
         px={1}
       >
+        <Header/>
         <Footer />
       </Stack>
 
