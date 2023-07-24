4e8e09a7c0494f555b848b54b8c127b0d50778bb
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-07-25 02:29:15 +0900

    feat: 로그인 후 마이페이지에서 회원 정보를 조회할 수 있는 기능 구현

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,54 +1,40 @@
 // src/mocks/handlers.js
 import {rest} from 'msw'
+import {getSessionStorage, setSessionStorage} from "../utils/storage.ts";
 
 export const handlers = [
-  rest.post('/login', (req, res, ctx) => {
-    // Persist user's authentication in the session
-    sessionStorage.setItem('is-authenticated', 'true')
-
-    return res(
-      // Respond with a 200 status code
-      ctx.status(200),
-    )
-  }),
-
-  rest.get('/user', (req, res, ctx) => {
-    // Check if the user is authenticated in this session
-    const isAuthenticated = sessionStorage.getItem('is-authenticated')
-
-    if (!isAuthenticated) {
-      // If not authenticated, respond with a 403 error
-      return res(
-        ctx.status(403),
-        ctx.json({
-          errorMessage: 'Not authorized',
-        }),
-      )
-    }
-
-    // If authenticated, return a mocked user details
-    return res(
-      ctx.status(200),
-      ctx.json({
-        username: 'admin',
-      }),
-    )
-  }),
 
   rest.post('/fin-the-pen-web/sign-in', (req, res, ctx) => {
     alert('서버가 정상적으로 동작하지 않지만 게스트 모드로 로그인 합니다.')
+    setSessionStorage('is-authenticated', true); //JWT나 Basic으로 이전 예정
 
     return res(
       ctx.status(200),
       ctx.delay(1000),
-      ctx.json({
-        id: -1,
-        user_id: 'guest@finthepen.com',
-        name: '게스트',
-        bday: '2023-07-25',
-        registerDate: '2023-01-01',
-        phone_number: '010-1234-5678'
-      })
+    )
+  }),
+
+  rest.post('/fin-the-pen-web/user', (req, res, ctx) => {
+    const isAuthenticated = getSessionStorage<boolean>('is-authenticated', false)
+    console.log(isAuthenticated)
+    if (isAuthenticated) {
+      console.log('is-authenticated')
+      return res(
+        ctx.status(200),
+        ctx.delay(1000),
+        ctx.json({
+          id: -1,
+          user_id: 'guest@finthepen.com',
+          name: '게스트',
+          bday: '2023-07-25',
+          registerDate: '2023-01-01',
+          phone_number: '010-1234-5678'
+        })
+      )
+    }
+    return res(
+      ctx.status(403),
+      ctx.delay(1000),
     )
   }),
 
---
+++ src/types/index.ts
@@ -0,0 +1,8 @@
+export interface User {
+  id: number;
+  user_id: string;
+  name: string;
+  bday: string;
+  registerDate: string;
+  phone_number: string;
+}
