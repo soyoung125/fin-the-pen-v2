993a478a4a318255668025a3fc137f6d78da8e9c
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-08-06 01:48:48 +0900

    refactor: scheduleDrawerOpenAtom으로 ScheduleDrawer를 여닫을 수 있도록 개선

---
+++ src/app/recoil/scheduleDrawerOpenAtom.ts
@@ -0,0 +1,6 @@
+import { atom } from "recoil";
+
+export const scheduleDrawerOpenAtom = atom({
+  key: "scheduleDrawerOpenState",
+  default: false,
+});
--- src/containers/home/common/BottomBar.tsx
+++ src/containers/home/common/BottomBar.tsx
@@ -1,80 +1,106 @@
-import { BottomNavigation, BottomNavigationAction, Drawer, Paper } from '@mui/material';
-import SettingsIcon from '@mui/icons-material/Settings';
-import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
-import DataSaverOffIcon from '@mui/icons-material/DataSaverOff';
-import AddCircleIcon from '@mui/icons-material/AddCircle';
-import PaidIcon from '@mui/icons-material/Paid';
+import {
+  BottomNavigation,
+  BottomNavigationAction,
+  Drawer,
+  Paper,
+} from "@mui/material";
+import SettingsIcon from "@mui/icons-material/Settings";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+import DataSaverOffIcon from "@mui/icons-material/DataSaverOff";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import PaidIcon from "@mui/icons-material/Paid";
 import moment from "moment";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import PATH from '../../../domain/constants/path';
+import PATH from "../../../domain/constants/path";
+import { useRecoilState } from "recoil";
+import { scheduleDrawerOpenAtom } from "../../../app/recoil/scheduleDrawerOpenAtom.ts";
 
 function BottomBar() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const [drawerWidth, setDrawerWidth] = useState(0);
-    const [startTime, setStartTime] = useState('09');
+  const [drawerWidth, setDrawerWidth] = useState(0);
+  const [startTime, setStartTime] = useState("09");
 
-    // 전역 상태로 관리할 변수들
-    const [date, setDate] = useState(moment());
-    const [bottomDrawerOpen, setBottomDrawerOpen] = useState(false);
-    const [bottomDrawerTabMenu, setBottomDrawerTabMenu] = useState(0);
+  // 전역 상태로 관리할 변수들
+  const [date, setDate] = useState(moment());
+  const [bottomDrawerOpen, setBottomDrawerOpen] = useRecoilState(
+    scheduleDrawerOpenAtom
+  );
+  const [bottomDrawerTabMenu, setBottomDrawerTabMenu] = useState(0);
 
-    useEffect(() => {
-        if (moment().isSame(date, 'day')) {
-            setStartTime(moment().add(1, 'hours').format('HH'));
-        } else {
-            setStartTime(moment('09:00', 'HH:mm').format('HH'));
-        }
-    }, [date]);
+  useEffect(() => {
+    if (moment().isSame(date, "day")) {
+      setStartTime(moment().add(1, "hours").format("HH"));
+    } else {
+      setStartTime(moment("09:00", "HH:mm").format("HH"));
+    }
+  }, [date]);
 
-    return (
-        <>
-            <Paper
-                sx={{
-                    position: 'fixed',
-                    bottom: 0,
-                    left: 0,
-                    right: 0,
-                    paddingBottom: 1,
-                    zIndex: 1000,
-                }}
-                elevation={3}
-            >
-                <BottomNavigation
-                    value={bottomDrawerTabMenu}
-                    onChange={(event, newValue) => {
-                        setBottomDrawerTabMenu(newValue);
-                    }}
-                >
-                    <BottomNavigationAction
-                        label="홈"
-                        icon={<CalendarMonthIcon />}
-                        onClick={() => {
-                            // dispatch(changeViewMode('schedule'));
-                            navigate(PATH.home);
-                        }}
-                    />
-                    <BottomNavigationAction label="리포트" icon={<DataSaverOffIcon />} onClick={() => navigate(PATH.analysis)} />
-                    <BottomNavigationAction label="" icon={<AddCircleIcon />} onClick={() => setBottomDrawerOpen(true)} />
-                    <BottomNavigationAction label="자산관리" icon={<PaidIcon />} onClick={() => navigate(PATH.assetManagement)} />
-                    <BottomNavigationAction label="설정" icon={<SettingsIcon />} onClick={() => navigate(PATH.settings)} />
-                </BottomNavigation>
-            </Paper>
-            <Drawer
-                open={bottomDrawerOpen}
-                anchor="bottom"
-                onClose={() => setBottomDrawerOpen(false)}
-                // Drawer를 가운데로 위치할 수 있도록 도와줌. resize는 이후 업데이트 예정
-                PaperProps={{
-                    sx: {
-                        maxWidth: '400px',
-                        marginX: drawerWidth === 400 ? `calc((100% - ${drawerWidth}px)/2)` : null,
-                    },
-                }}
-            >
-                {/* 이 부분을 범용적으로 사용할 수 있게 만드는 건 어떨까? */}
-                {/* <ScheduleDrawer
+  return (
+    <>
+      <Paper
+        sx={{
+          position: "fixed",
+          bottom: 0,
+          left: 0,
+          right: 0,
+          paddingBottom: 1,
+          zIndex: 1000,
+        }}
+        elevation={3}
+      >
+        <BottomNavigation
+          value={bottomDrawerTabMenu}
+          onChange={(event, newValue) => {
+            setBottomDrawerTabMenu(newValue);
+          }}
+        >
+          <BottomNavigationAction
+            label="홈"
+            icon={<CalendarMonthIcon />}
+            onClick={() => {
+              // dispatch(changeViewMode('schedule'));
+              navigate(PATH.home);
+            }}
+          />
+          <BottomNavigationAction
+            label="리포트"
+            icon={<DataSaverOffIcon />}
+            onClick={() => navigate(PATH.analysis)}
+          />
+          <BottomNavigationAction
+            label=""
+            icon={<AddCircleIcon />}
+            onClick={() => setBottomDrawerOpen(true)}
+          />
+          <BottomNavigationAction
+            label="자산관리"
+            icon={<PaidIcon />}
+            onClick={() => navigate(PATH.assetManagement)}
+          />
+          <BottomNavigationAction
+            label="설정"
+            icon={<SettingsIcon />}
+            onClick={() => navigate(PATH.settings)}
+          />
+        </BottomNavigation>
+      </Paper>
+      <Drawer
+        open={bottomDrawerOpen}
+        anchor="bottom"
+        onClose={() => setBottomDrawerOpen(false)}
+        // Drawer를 가운데로 위치할 수 있도록 도와줌. resize는 이후 업데이트 예정
+        PaperProps={{
+          sx: {
+            maxWidth: "400px",
+            marginX:
+              drawerWidth === 400 ? `calc((100% - ${drawerWidth}px)/2)` : null,
+          },
+        }}
+      >
+        {/* 이 부분을 범용적으로 사용할 수 있게 만드는 건 어떨까? */}
+        {/* <ScheduleDrawer
                     setDrawerWidth={setDrawerWidth}
                     handleClose={() => dispatch(setBottomDrawerOpenFalse())}
                     data={{
@@ -82,13 +108,13 @@ function BottomBar() {
                     }}
                     mode={SCHEDULE_DRAWER_MODE.create}
                 /> */}
-                <>ScheduleDrawer</>
-            </Drawer>
-
-        </>
-    );
+        <>ScheduleDrawer</>
+      </Drawer>
+    </>
+  );
 }
+
 export default BottomBar;
-  /**
-* 하단 바
-*/
+/**
+ * 하단 바
+ */
